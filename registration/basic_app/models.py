4039cf3b89f16dc69b2790b6eff3f2c8b9abf09c
Shyrosky <60220242+Shyrosky@users.noreply.github.com>
2020-06-26 18:15:43 +0200

    first

---
+++ registration/basic_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class UserProfileInfo(models.Model):
+    # create relationship (don't inherit from Use!)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Add any additional attributes you want
+    portfolio = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_pics',blank=True)
+    
+    def __str__(self):
+        return self.user.username
+
+        # Built-in attribute of django.contrib.auth.models.User!
